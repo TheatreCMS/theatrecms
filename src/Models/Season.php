97eab7d17d015a71eed537218effdcf088df53e8
Daryl <daryl@clubdeuce.net>
2025-12-13 19:09:16 -0500

    Add Season model and SeasonRepository

---
+++ src/Models/Season.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Clubdeuce\Theaterpress\Models;
+
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\Table;
+
+#[Entity, Table(name: 'seasons')]
+class Season implements \JsonSerializable
+{
+    #[Id, Column(type: 'integer'), GeneratedValue(strategy: 'AUTO')]
+    private int $id;
+
+    #[Column(type: 'string', nullable: false)]
+    private string $slug;
+
+    #[Column(type: 'string', nullable: false)]
+    private string $label;
+
+    public function __construct(string $slug, string $label)
+    {
+        $this->slug = $slug;
+        $this->label = $label;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getSlug(): string
+    {
+        return $this->slug;
+    }
+
+    public function getLabel(): string
+    {
+        return $this->label;
+    }
+
+    public function jsonSerialize(): mixed
+    {
+        return [
+            'id' => $this->getId(),
+            'slug' => $this->getSlug(),
+            'label' => $this->getLabel(),
+        ];
+    }
+
+}
---
+++ src/Repositories/SeasonRepository.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Clubdeuce\Theaterpress\Repositories;
+
+use Clubdeuce\Theaterpress\Models\Season;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Exception\ORMException;
+
+readonly class SeasonRepository
+{
+    public function __construct(private EntityManager $em) {}
+
+    /**
+     * @throws ORMException
+     */
+    public function create(Season $season): Season
+    {
+        $this->em->persist($season);
+        $this->em->flush();
+        return $season;
+    }
+
+    public function findById(int $id)
+    {
+        return $this->em->find('Clubdeuce\Theaterpress\Models\Season', $id);
+    }
+
+}
