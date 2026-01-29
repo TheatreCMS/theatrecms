16cb5e273d3c2bc252d29f489c3147553732ef5e
Daryl <daryl@clubdeuce.net>
2026-01-29 18:55:55 -0500

    Refactor `PersonRepository` to add `save` method, `fetchAll` and `fetch` methods, improving data handling and repository functionality.

--- src/Repositories/PersonRepository.php
+++ src/Repositories/PersonRepository.php
@@ -5,6 +5,7 @@ namespace Clubdeuce\TheatreCMS\Repositories;
 use Clubdeuce\TheatreCMS\Models\Person;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Exception\ORMException;
+use Doctrine\ORM\OptimisticLockException;
 
 final readonly class PersonRepository
 {
@@ -12,15 +13,34 @@ final readonly class PersonRepository
     {
     }
 
-    /**
-     * @throws ORMException
-     */
     public function create(string $name, string $biography = '', string $headshotUrl = ''): Person
     {
         $person = new Person($name, $biography, $headshotUrl);
-        $this->em->persist($person);
-        $this->em->flush();
+        return $this->save($person);
+    }
 
-        return $person;
+    public function save(Person $person): bool|Person
+    {
+        try {
+            $this->em->persist($person);
+            $this->em->flush();
+            return $person;
+        } catch (ORMException $e) {
+            trigger_error($e->getMessage(), E_USER_WARNING);
+            return false;
+        }
+    }
+
+    /**
+     * @return array<Person>
+     */
+    public function fetchAll(): array
+    {
+        return $this->em->getRepository(Person::class)->findAll();
+    }
+
+    public function fetch(int $id): ?Person
+    {
+        return $this->em->getRepository(Person::class)->find($id);
     }
 }
