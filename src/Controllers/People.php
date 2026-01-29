59e0c670929868f929cb00fa3ec4fa12fb7f34e5
Daryl <daryl@clubdeuce.net>
2026-01-29 18:56:45 -0500

    Add `People` controller to handle person-related API endpoints
    - Implement `create`, `fetchAll`, and `fetch` methods for managing person data.
    - Add `parseArgs` helper for argument parsing with defaults.
    - Integrate `PersonRepository` to interact with the database.

---
+++ src/Controllers/People.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * The person endpoint controller.
+ */
+namespace Clubdeuce\TheatreCMS\Controllers;
+
+use Clubdeuce\TheatreCMS\Repositories\PersonRepository;
+use Psr\Http\Message\RequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+class People
+{
+    public function __construct(private PersonRepository $personRepository)
+    {
+    }
+
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @return Response
+     */
+    public function create(Request $request, Response $response): Response
+    {
+        $args = $request->getBody()->getContents();
+
+        if (empty($args)) {
+            $response->getBody()->write('{"error": "Empty POST body."}');
+            return $response->withStatus(400);
+        }
+
+        $args = json_decode($args, true);
+
+        $data = $this->parseArgs($args, [
+            'name' => '',
+            'biography' => '',
+            'headshotUrl' => ''
+        ]);
+
+        $person = $this->personRepository->create($data['name'], $data['biography'], $data['headshotUrl']);
+        $params = json_encode($person);
+
+        if ($params)
+            $response->getBody()->write($params);
+
+        return $response;
+    }
+
+    public function fetchAll(Request $request, Response $response): Response
+    {
+        $people = $this->personRepository->fetchAll();
+        $data = json_encode($people);
+
+        if ($data) {
+            $response->getBody()->write($data);
+        }
+
+        return $response;
+    }
+
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @param string[]|float[]|int[] $args
+     * @return Response
+     */
+    public function fetch(Request $request, Response $response, array $args = []): Response
+    {
+        $id = intval($args['id']);
+
+        $person = $this->personRepository->fetch($id);
+        $data = json_encode($person);
+
+        if($data)
+            $response->getBody()->write($data);
+
+        return $response;
+    }
+
+    /**
+     * @param array<string, mixed> $args
+     * @param array<string, mixed> $defaults
+     * @return array<string, mixed>
+     */
+    private function parseArgs(array $args, array $defaults): array
+    {
+        foreach($defaults as $name => $default) {
+            if(!array_key_exists($name, $args)) {
+                $args[$name] = $default;
+            }
+        }
+
+        return $args;
+    }
+}
