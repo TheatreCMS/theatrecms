92425ab2e42a176b4645a74637c229ed29df79a6
Daryl <daryl@clubdeuce.net>
2026-01-30 14:21:13 -0500

    Update `require_once` path in `index.php` to use `dirname(__DIR__)` for better directory resolution

--- www/index.php
+++ www/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once "../vendor/autoload.php";
+require_once dirname(__DIR__) . "/vendor/autoload.php";
 
 use Clubdeuce\TheatreCMS\Models\Season;
 use Clubdeuce\TheatreCMS\Repositories\SeasonRepository;
