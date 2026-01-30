1ad5ddb64f3cdf8aff3b94a7d8c2a6c7e6e3376d
Daryl <daryl@clubdeuce.net>
2026-01-30 15:09:08 -0500

    Add PHPStan task to `RoboFile.php` for static code analysis

--- RoboFile.php
+++ RoboFile.php
@@ -5,14 +5,21 @@
  * @see https://robo.li/
  */
 use Robo\Tasks;
+use Sweetchuck\Robo\Phpstan;
 
 class RoboFile extends Tasks
 {
+
     // define public methods as commands
-    public function tests()
+    public function tests(): void
     {
         $this->taskPHPUnit()
             ->configFile('phpunit.xml.dist')
             ->run();
+    }
+
+    public function phpstan(): void
+    {
+        $this->taskExec('./vendor/bin/phpstan analyse -c phpstan.neon.dist --memory-limit=1G')->run();
     }
 }
