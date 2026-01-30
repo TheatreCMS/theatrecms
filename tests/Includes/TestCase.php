42f2d83a24b9d0cb5e47eedf9c624c107c2506af
Daryl <daryl@clubdeuce.net>
2026-01-29 23:03:23 -0500

    Set up PHPUnit testing framework with initial configuration and bootstrap files

---
+++ tests/Includes/TestCase.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Clubdeuce\TheatreCMS\Tests\Includes;
+
+class TestCase extends \PHPUnit\Framework\TestCase
+{
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+define( 'SRC_DIR', dirname(__DIR__));
+
+require_once SRC_DIR . '/vendor/autoload.php';
+require_once SRC_DIR . '/tests/Includes/TestCase.php';
