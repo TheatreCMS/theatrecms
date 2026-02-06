6884721dd531a5ad2853130fb0c5ff941c1afae6
Daryl <daryl@clubdeuce.net>
2026-02-06 12:47:21 -0500

    Add `create` method to `WorkRepository` for creating new `Work` entities with error handling

--- src/Repositories/WorkRepository.php
+++ src/Repositories/WorkRepository.php
@@ -4,6 +4,7 @@ namespace Clubdeuce\TheatreCMS\Repositories;
 
 use Clubdeuce\TheatreCMS\Models\Work;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Exception\ORMException;
 
 readonly class WorkRepository
 {
@@ -19,5 +20,22 @@ readonly class WorkRepository
     public function fetch(int $id): ?Work
     {
         return $this->em->getRepository(Work::class)->find($id);
+    }
+
+    public function create(array $args): bool|Work
+    {
+        $work = new Work();
+
+        $work->setTitle($args['title'])
+            ->setDescription($args['synopsis'] ?? '');
+
+        try {
+            $this->em->persist($work);
+            $this->em->flush();
+            return $work;
+        } catch (ORMException $e) {
+            trigger_error($e->getMessage(), E_USER_WARNING);
+            return false;
+        }
     }
 }
