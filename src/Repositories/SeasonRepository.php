a3fd1a5e09a8e720610554c2dfd1359b27a6e1b5
Daryl <daryl@clubdeuce.net>
2026-02-06 16:21:53 -0500

    Refactor `SeasonRepository`: extend `Base`, streamline `create` method, and remove redundant methods

--- src/Repositories/SeasonRepository.php
+++ src/Repositories/SeasonRepository.php
@@ -7,63 +7,20 @@ use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Exception\ORMException;
 use Doctrine\ORM\OptimisticLockException;
 
-readonly class SeasonRepository
+class SeasonRepository extends Base
 {
-    public function __construct(private EntityManager $em)
-    {
-    }
+     protected string $entityClass = Season::class;
 
-    /**
-     * @throws ORMException
-     */
-    public function create(Season $season): Season
-    {
+     public function create(array $args): Season
+     {
+        $season = new Season($args['slug'], $args['label']);
+
+        $season->setStartDate($args['startDate'])
+            ->setOverview($args['overview']);
+
         $this->em->persist($season);
         $this->em->flush();
+
         return $season;
-    }
-
-    public function findById(int $id): ?Season
-    {
-        try {
-            return $this->em->find('Clubdeuce\TheatreCMS\Models\Season', $id);
-        } catch (OptimisticLockException | ORMException $e) {
-            trigger_error($e->getMessage(), E_USER_WARNING);
-            return null;
-        }
-    }
-
-    /**
-     * @return Season[]
-     */
-    public function findAll(): array
-    {
-        $repository = $this->em->getRepository(Season::class);
-        return $repository->findAll();
-    }
-
-    public function deleteById(int $id): bool
-    {
-        try {
-            if ($item = $this->em->find(Season::class, $id)) {
-                $this->em->remove($item);
-            }
-
-            $this->em->flush();
-            return true;
-        } catch (ORMException $e) {
-            trigger_error($e->getMessage(), E_USER_WARNING);
-            return false;
-        }
-    }
-
-    public function update(Season $season): void
-    {
-        try {
-            $this->em->persist($season);
-            $this->em->flush();
-        } catch (ORMException $e) {
-            trigger_error($e->getMessage(), E_USER_WARNING);
-        }
-    }
+     }
 }
