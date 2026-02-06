02948bd67064ffbf6b93af9488522503a2f48a88
Daryl <daryl@clubdeuce.net>
2026-02-06 14:17:17 -0500

    Add abstract `Base` repository with common query and fetch methods

---
+++ src/Repositories/Base.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Clubdeuce\TheatreCMS\Repositories;
+
+use Clubdeuce\TheatreCMS\Models\Work;
+use Doctrine\ORM\EntityManagerInterface;
+
+abstract class Base
+{
+    protected string $entityClass;
+
+    public function __construct(protected EntityManagerInterface $em)
+    {
+    }
+
+    public function query(array $args = []): array
+    {
+        return $this->em->getRepository($this->entityClass)->findBy($args, ['title' => 'ASC'], 10, null);
+    }
+
+    public function fetchAll(): array
+    {
+        return $this->em->getRepository($this->entityClass)->findAll();
+    }
+
+    public function fetch(int $id): ?Work
+    {
+        return $this->em->getRepository(Work::class)->find($id);
+    }
+
+    abstract public function create(array $args);
+
+}
