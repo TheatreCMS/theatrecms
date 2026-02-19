5e8cf9d5356b1eaeeb808164fde997d4035038be
Daryl <daryl@clubdeuce.net>
2026-02-19 09:39:58 -0500

    Refactor: Remove API routes and introduce Twig test route
    - Replaced extensive API route logic for `/api` endpoints with a simple `/twig-test` route to demonstrate Twig integration.
    - Removed controllers and repositories tied to `Season` and `Works` API routes.

--- www/index.php
+++ www/index.php
@@ -1,13 +1,10 @@
 <?php
 require_once dirname(__DIR__) . "/vendor/autoload.php";
 
-use Clubdeuce\TheatreCMS\Controllers\WorksController;
-use Clubdeuce\TheatreCMS\Models\Season;
-use Clubdeuce\TheatreCMS\Repositories\SeasonRepository;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
-use Slim\Routing\RouteCollectorProxy;
+use Slim\Views\Twig;
 
 /**
  * @var Psr\Container\ContainerInterface $container
@@ -24,92 +21,17 @@ $app->get('/', function (Request $request, Response $response) {
     return $response;
 });
 
-$app->group('/api', function (RouteCollectorProxy $group) {
-    $group->get('/people', [Clubdeuce\TheatreCMS\Controllers\People::class, 'fetchAll']);
-    $group->post('/people', [Clubdeuce\TheatreCMS\Controllers\People::class, 'create']);
-    $group->get('/people/{id}', [Clubdeuce\TheatreCMS\Controllers\People::class, 'fetch']);
-    $group->put('/people/{id}', [Clubdeuce\TheatreCMS\Controllers\People::class, 'update']);
-    $group->delete('/people/{id}', [Clubdeuce\TheatreCMS\Controllers\People::class, 'delete']);
+// Simple Twig test route. Renders templates/test.twig using the Twig service from the container.
+$app->get('/twig-test', function (Request $request, Response $response) use ($container) {
+    if (!$container->has(Twig::class)) {
+        $response->getBody()->write('Twig not available');
+        return $response->withStatus(500);
+    }
 
-    $group->group('/seasons', function (RouteCollectorProxy $group) {
-        $group->post('', function (Request $request, Response $response) {
-            /** @var SeasonRepository $seasonRepository */
-            $seasonRepository = $this->get(SeasonRepository::class);
+    /** @var Twig $twig */
+    $twig = $container->get(Twig::class);
 
-            $data = (array)$request->getParsedBody();
-
-            if(empty($data)) {
-                // there was no JSON body in the POST request
-                $response->getBody()->write('{"error": "Empty JSON body."}');
-                return $response->withHeader('Content-Type', 'application/json')->withStatus(400);
-            }
-
-            $season = new Season($data['slug'], $data['label']);
-            $seasonRepository->create($season);
-
-            $params = json_encode($season);
-
-            if ($params)
-                $response->getBody()->write($params);
-
-            return $response->withHeader('Content-Type', 'application/json')->withStatus(201);
-        });
-
-        $group->get('', function (Request $request, Response $response) {
-            /** @var SeasonRepository $seasonRepository */
-            $seasonRepository = $this->get(SeasonRepository::class);
-            $seasons = $seasonRepository->fetchAll();
-
-            $data = json_encode($seasons);
-
-            if ($data)
-                $response->getBody()->write($data);
-
-            return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
-        });
-
-        $group->delete('/{id}', function (Request $request, Response $response, array $args) {
-            $id = filter_var($args['id'], FILTER_VALIDATE_INT);
-
-            if($id === false) {
-                $response->getBody()->write('{"error": "Invalid season ID."}');
-                return $response->withHeader('Content-Type', 'application/json')->withStatus(400);
-            }
-
-            /** @var SeasonRepository $seasonRepository */
-            $seasonRepository = $this->get(SeasonRepository::class);
-
-            $seasonRepository->deleteById($id);
-
-            $message = json_encode(['message' => "Season {$id} deleted."]);
-
-            if ($message)
-                $response->getBody()->write($message);
-
-            return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
-        });
-
-        $group->get('/{id}', function (Request $request, Response $response, array $args) {
-            $id = filter_var($args['id'], FILTER_VALIDATE_INT);
-            /** @var SeasonRepository $seasonRepository */
-            $seasonRepository = $this->get(SeasonRepository::class);
-            $season = $seasonRepository->findById($id);
-            if($season === null) {
-                $response->getBody()->write(json_encode(['error' => "Season {$id} not found."]));
-                return $response->withHeader('Content-Type', 'application/json')->withStatus(404);
-            }
-
-            $response->getBody()->write(json_encode($season));
-            return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
-        });
-    });
-    $group->group( '/works', function (RouteCollectorProxy $group) {
-        $group->get('', [WorksController::class, 'get']);
-        $group->post('', [WorksController::class, 'create']);
-        $group->get( '/{id}', [WorksController::class, 'getById']);
-        $group->put('/{id}', [WorksController::class, 'update']);
-        $group->delete('/{id}', [WorksController::class, 'delete']);
-    });
+    return $twig->render($response, 'test.twig', ['name' => 'Daryl']);
 });
 
 $app->run();
