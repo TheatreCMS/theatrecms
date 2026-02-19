a8226adce492a19d697f48cc95ddefd3531d82f7
Daryl <daryl@clubdeuce.net>
2026-02-19 10:07:07 -0500

    Remove `/twig-test` route
    - Deleted the `/twig-test` route and its associated Twig rendering logic from `index.php`.

--- www/index.php
+++ www/index.php
@@ -33,17 +33,4 @@ $app->get('/admin', function (Request $request, Response $response) use ($contai
     return $twig->render($response, 'layouts/admin.html.twig');
 });
 
-// Simple Twig test route. Renders templates/test.twig using the Twig service from the container.
-$app->get('/twig-test', function (Request $request, Response $response) use ($container) {
-    if (!$container->has(Twig::class)) {
-        $response->getBody()->write('Twig not available');
-        return $response->withStatus(500);
-    }
-
-    /** @var Twig $twig */
-    $twig = $container->get(Twig::class);
-
-    return $twig->render($response, 'test.twig', ['name' => 'Daryl']);
-});
-
 $app->run();
