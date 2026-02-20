db1991a02767f6c285f901ecc0d3af900da350d5
Daryl <daryl@clubdeuce.net>
2026-02-19 22:15:54 -0500

    Add admin season management routes with Twig rendering
    - Added `/admin/seasons` route for listing all seasons.
    - Added `/admin/seasons/edit/{id}` route for editing specific seasons.
    - Added `SeasonRepository` usage for fetching seasons in routes.
    - Included Twig templates for rendering season-related admin pages.

--- www/index.php
+++ www/index.php
@@ -3,6 +3,7 @@ require_once dirname(__DIR__) . "/vendor/autoload.php";
 
 use Clubdeuce\TheatreCMS\Controllers\SeasonController;
 use Clubdeuce\TheatreCMS\Middleware\RequireTwigMiddleware;
+use Clubdeuce\TheatreCMS\Repositories\SeasonRepository;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
@@ -37,4 +38,28 @@ $app->get('/admin/seasons/create', function (Request $request, Response $respons
     return $twig->render($response, 'admin/seasons/create.html.twig');
 })->add(new RequireTwigMiddleware($container));
 
+$app->get('/admin/seasons/edit/{id}', function (Request $request, Response $response) use ($container) {
+    $repository = $container->get(SeasonRepository::class);
+    /** @var SeasonRepository $repository */
+    $season = $repository->fetch($request->getAttribute('id'));
+
+    /** @var Twig $twig */
+    $twig = $container->get(Twig::class);
+
+    return $twig->render($response, 'admin/seasons/edit.html.twig', ['season' => $season]);
+})->add(new RequireTwigMiddleware($container));
+
+$app->get('/admin/seasons', function (Request $request, Response $response) use ($container) {
+    /** @var Twig $twig */
+    $twig = $container->get(Twig::class);
+
+    /** @var SeasonController $seasonController */
+    $seasonController = $container->get(SeasonController::class);
+    $seasons = $seasonController->repository()->fetchAll();
+
+    return $twig->render($response, 'admin/seasons/index.html.twig', ['seasons' => $seasons]);
+})->add(new RequireTwigMiddleware($container));
+
+$app->post('/admin/seasons/edit', [SeasonController::class, 'update']);
+$app->post('/admin/seasons', [SeasonController::class, 'store']);
 $app->run();
