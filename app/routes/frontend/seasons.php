378a949f2c4b737b42cd6247d28026bb7b37e16c
Daryl <daryl@clubdeuce.net>
2026-03-06 16:59:13 -0500

    Integrate `TemplateResolver` into season routes for dynamic template resolution.

--- app/routes/frontend/seasons.php
+++ app/routes/frontend/seasons.php
@@ -3,12 +3,17 @@
 use Clubdeuce\TheatreCMS\Middleware\RequireTwigMiddleware;
 use Clubdeuce\TheatreCMS\Repositories\ProductionRepository;
 use Clubdeuce\TheatreCMS\Repositories\SeasonRepository;
+use Clubdeuce\TheatreCMS\Theme\TemplateResolver;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
 
+/**
+ * @var TemplateResolver $resolver
+ */
+
 if (isset($app)) {
-    $app->group('/seasons', function ($group) {
+    $app->group('/seasons', function ($group) use ($resolver){
         $container = $group->getContainer();
 
         $group->get('/{slug}/{productionSlug}', function (Request $request, Response $response, array $args) use ($container) {
@@ -25,7 +30,8 @@ if (isset($app)) {
 
             return $twig->render($response, 'seasons/production.html.twig', ['production' => $production]);
         });
-        $group->get('/{slug}', function (Request $request, Response $response) use ($container) {
+
+        $group->get('/{slug}', function (Request $request, Response $response) use ($container, $resolver) {
             $repository = $container->get(SeasonRepository::class);
             /** @var SeasonRepository $repository */
             $season = $repository->fetchBySlug($request->getAttribute('slug'));
@@ -33,7 +39,13 @@ if (isset($app)) {
             /** @var Twig $twig */
             $twig = $container->get(Twig::class);
 
-            return $twig->render($response, 'seasons/show.html.twig', ['season' => $season]);
+            $template = $resolver->resolve($twig,
+                'seasons/single-' . $season->getSlug() . '.html.twig',  // most specific
+                'seasons/single.html.twig',
+                'index.html.twig'                                         // fallback
+            );
+
+            return $twig->render($response, $template, ['season' => $season]);
         });
 
         $group->get('', function (Request $request, Response $response) use ($container) {
