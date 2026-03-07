e489d8ef95e186cc7a4c7268a4f8e6c0e3717e69
Daryl <daryl@clubdeuce.net>
2026-03-07 11:14:03 -0500

    Add documentation for `TemplateResolver` and its template hierarchy logic

--- src/Theme/TemplateResolver.php
+++ src/Theme/TemplateResolver.php
@@ -4,6 +4,16 @@ namespace TheatreCMS\Theme;
 
 use Slim\Views\Twig;
 
+/**
+ * Resolves a template from a list of candidates, returning the first one that exists. This is used
+ * to implement the template hierarchy, where more specific templates are checked before more generic ones.
+ *
+ * For example, when rendering a single production, the candidates might be:
+ * - productions/single-{production-slug}.html.twig
+ * - productions/single.html.twig
+ * - single.html.twig
+ * - index.html.twig
+ */
 class TemplateResolver
 {
     public function resolve(Twig $twig, string ...$candidates): string
