25975687ffe00fa3eeb5bdece5e99b23b780b6df
Daryl <daryl@clubdeuce.net>
2026-03-25 13:51:26 -0400

    Integrate `PostRepository` and frontend `posts.php` routes; display posts on the homepage.

--- www/index.php
+++ www/index.php
@@ -21,6 +21,7 @@ require_once ROOT_DIR . "/vendor/autoload.php";
 
 use TheatreCMS\Controllers\LoginController;
 use TheatreCMS\Middleware\RequireTwigMiddleware;
+use TheatreCMS\Repositories\PostRepository;
 use TheatreCMS\Theme\TemplateResolver;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -50,6 +51,7 @@ require ROUTES_DIR . '/admin/venues.php';
 require ROUTES_DIR . '/admin/works.php';
 require ROUTES_DIR . '/admin/posts.php';
 require ROUTES_DIR . '/frontend/seasons.php';
+require ROUTES_DIR . '/frontend/posts.php';
 
 $app->get('/admin/login', [LoginController::class, 'login']);
 $app->post('/admin/login', [LoginController::class, 'authenticate']);
@@ -62,11 +64,10 @@ $app->get('/admin', function (Request $request, Response $response) use ($contai
     return $twig->render($response, 'admin/index.html.twig');
 })->add(new RequireTwigMiddleware($container));
 
-
-
 $app->get('/', function (Request $request, Response $response) use ($container) {
     $twig = $container->get(Twig::class);
+    $posts = $container->get(PostRepository::class)->fetchAll();
 
-    return $twig->render($response, 'index.html.twig');
+    return $twig->render($response, 'index.html.twig', ['posts' => $posts]);
 });
 $app->run();
